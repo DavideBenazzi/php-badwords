84b4bcee558f49cfee327622eb9e430837e847b6
Davide Benazzi <benazzi.davide@gmail.com>
2020-12-14 15:42:00 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+</head>
+<body>
+
+<?php 
+    $name = 'Davide';
+
+?>
+
+<h1>
+    Hello World , <?php echo $name ?>
+</h1>
+    
+</body>
+</html>

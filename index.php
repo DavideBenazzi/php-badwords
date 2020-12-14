e5505b26e7a60a03e81747ffbb6c7c3389cf2b7d
Davide Benazzi <benazzi.davide@gmail.com>
2020-12-14 16:20:23 +0100

    Replace of the string

--- index.php
+++ index.php
@@ -10,12 +10,27 @@
 <body>
 <!-- PHP -->
 <?php 
-    $paragraph = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis iure quam veritatis ea dolor et commodi ut odit aspernatur quod, sed sunt nam, quidem qui? Reprehenderit illo minima inventore atque?';
+    $paragraph = 'lorem ipsum, dolor sit amet consectetur adipisicing elit.lorem ipsum, dolor sit amet consectetur adipisicing elit.lorem ipsum, dolor sit amet consectetur adipisicing elit.Lorem ipsum, dolor sit amet consectetur adipisicing elit.Lorem ipsum, dolor sit amet consectetur adipisicing elit.';
+    $badword = 'dolor';
 ?>
+
 <!-- MY PARAGRAPH -->
 <h2 class="mb20">My Paragraph with PHP</h2>
-<p class="mb20"><?php echo $paragraph ?></p>
-<h3 class="mb20">Length of paragraph : <?php echo strlen($paragraph) ?></h3>
+
+<!-- ECHO PARAGRAPH -->
+<p class="mb20"><?php echo $paragraph; ?></p>
+
+<!-- LENGTH PARAGRAPH -->
+<h3 class="mb20">Length of paragraph : <?php echo strlen($paragraph); ?></h3>
+
+<!-- REPLACE OF PART OF THE STRING -->
+<?php $bad_word = $_GET['word'] ?>
+<?php $new_paragraph = str_replace($bad_word , '*****' , $paragraph); ?>
+
+<!-- SOSTITUTION OF 'WORD' WITH '*****' -->
+<h3 class="mb20">Type in the Url "?word=" and the word you want to censure</h3>
+<p class="mb20"><?php echo $new_paragraph ?></p>
+
     
 </body>
 </html>

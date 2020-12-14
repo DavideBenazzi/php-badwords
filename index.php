9777a84e01e3a8ca5b0c00f66962159a904ac348
Davide Benazzi <benazzi.davide@gmail.com>
2020-12-14 15:56:36 +0100

    Length of paragraph

--- index.php
+++ index.php
@@ -3,18 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="stylesheet" href="./css/style.css">
     <title>php-badwords</title>
 </head>
 <body>
-
+<!-- PHP -->
 <?php 
-    $name = 'Davide';
-
+    $paragraph = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis iure quam veritatis ea dolor et commodi ut odit aspernatur quod, sed sunt nam, quidem qui? Reprehenderit illo minima inventore atque?';
 ?>
-
-<h1>
-    Hello World , <?php echo $name ?>
-</h1>
+<!-- MY PARAGRAPH -->
+<h2 class="mb20">My Paragraph with PHP</h2>
+<p class="mb20"><?php echo $paragraph ?></p>
+<h3 class="mb20">Length of paragraph : <?php echo strlen($paragraph) ?></h3>
     
 </body>
 </html>
